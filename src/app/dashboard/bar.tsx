b56335083aa7867647b6a776352b0b3403b17798
赵圻昀 <qiyun.zhao@baozun.com>
2024-05-22 11:29:54 +0800

    打字机

--- src/app/dashboard/bar.tsx
+++ src/app/dashboard/bar.tsx
@@ -2,10 +2,11 @@
 import React from "react";
 import useThemeStore from "@/store/themeStore";
 import Lottie from "react-lottie";
-import titlePng from './public/星球.json'
+import titlePng from "./public/星球.json";
+import Image from "next/image";
 // eslint-disable-next-line import/no-anonymous-default-export, react/display-name
 export default () => {
-  const { activeTheme } = useThemeStore((state) => state);
+  const { activeTheme, setTheme } = useThemeStore((state) => state);
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -16,15 +17,66 @@ export default () => {
   };
   return (
     <div
-      className={`theme-${activeTheme} text-primary h-8 px-20 bg-backgroundBar flex flex-col justify-center items-center`}
+      className={`theme-${activeTheme} text-white px-20 bg-backgroundBar flex justify-between `}
+      style={{ height: "75px" }}
     >
-      <div>
+      <div style={{ lineHeight: "75px" }}>
         <Lottie
           options={defaultOptions}
-          width={100}
-          height={100}
+          width={80}
+          height={80}
           isClickToPauseDisabled={true}
         />
+      </div>
+      <div
+        className="font-serif text-2xl font-semibold flex  justify-between w-1/4 items-center"
+        style={{ lineHeight: "75px" }}
+      >
+        <div className="cursor-pointer">实验室</div>
+        <div className="cursor-pointer">想法</div>
+        <div className="cursor-pointer">人间纪实</div>
+        <div
+          className={`w-20 h-10  border-solid border-2 rounded-lg border-indigo-900 flex justify-between bg-secondary truncate`}
+        >
+          <div
+            className={`cursor-pointer   p-1  ${
+              activeTheme === "solarizedLight"
+                ? "bg-sky-500 rounded-lg border-solid"
+                : ""
+            }`}
+            onClick={() => setTheme("solarizedLight")}
+          >
+            <Image
+              src={
+                activeTheme === "solarizedLight"
+                  ? "/bright2.svg"
+                  : "/bright1.svg"
+              }
+              alt="黑色未选中"
+              width={27}
+              height={27}
+            />
+          </div>
+          {/* <div
+            style={{ height: "100%", width: "2px" }}
+            className="bg-backgroundBar "
+          /> */}
+          <div
+            className={`cursor-pointer   p-1  ${
+              activeTheme === "nord"
+                ? "bg-neutral-700  rounded-s-lg border-solid"
+                : ""
+            }`}
+            onClick={() => setTheme("nord")}
+          >
+            <Image
+              src={activeTheme === "nord" ? "/dark2.svg" : "/dark1.svg"}
+              alt="黑色未选中"
+              width={27}
+              height={27}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/app/dashboard/home.tsx
+++ src/app/dashboard/home.tsx
@@ -1,6 +1,22 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useThemeStore from "@/store/themeStore";
+import useTypewriter from "react-typewriter-hook";
+import { tong } from "./public/tong";
+const Note = () => {
+  const [randomInteger, setRandomInteger] = useState<any>("1");
+  useEffect(() => {
+    // 每 15 秒更新一次随机整数，范围是 1 到 117
+    const intervalId = setInterval(() => {
+      setRandomInteger(1 + Math.floor(Math.random() * 116)); // 生成 1 到 117 之间的随机整数
+    }, 15000);
+
+    // 组件卸载时清除定时器
+    return () => clearInterval(intervalId);
+  }, []);
+  const talk = useTypewriter(`${tong[randomInteger]}`);
+  return <div className="text-primary text-xl subpixel-antialiased">{talk}</div>;
+};
 function App() {
   const { themes, activeTheme, setTheme } = useThemeStore((state) => state);
 
@@ -13,19 +29,7 @@ function App() {
         flex flex-col justify-center items-center
       `}
     >
-      <p className="mb-10 text-primary">当前主题：{activeTheme}</p>
-      <span className="text-primary/[0.5]">点击下方按钮切换主题</span>
-      <div className="mt-10">
-        {themes.map((theme, index) => (
-          <button
-            key={theme}
-            className="border rounded p-2 mr-5 bg-secondary text-white"
-            onClick={() => setTheme(theme)}
-          >
-            {theme}
-          </button>
-        ))}
-      </div>
+      <Note />
     </div>
   );
 }
