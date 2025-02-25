37d775f8cfefcdb77276b59b70a836b5e811fede
赵圻昀 <qiyun.zhao@baozun.com>
2025-02-25 17:26:46 +0800

    更改next

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  target: 'serverless',
   output: "export",
   images: {
     unoptimized: true,
     loader: 'custom',
     loaderFile: './image-loader.js'
-  },
-  // 关闭实验性功能
-  experimental: {
-    appDir: false
   }
 };
 
